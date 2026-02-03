90891bc323f641daab4efb7125423b796762ec58
mujcinov <85841489+mujcinov@users.noreply.github.com>
2026-02-03 11:26:26 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,33 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className="hero"></div>
+      <div
+        className="hero"
+      >
+        <header className="top-bar">
+          <div className="logo">LOGO</div>
+
+          <nav className="menu">
+            <a href="#">Destinacije</a>
+            <a href="#">Putovanja</a>
+            <a href="#">Kontakt</a>
+          </nav>
+
+          <div className="actions">
+            <div className="call">
+              <span className="call-icon">📞</span>
+              <span className="call-text">Pozovite nas</span>
+            </div>
+            <button className="cta">Putujte s nama</button>
+            <div className="hamburger">☰</div>
+          </div>
+        </header>
+      </div>
+
       <div className="content"></div>
     </>
   );
 }
 
 export default App;
+
