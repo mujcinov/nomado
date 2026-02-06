e75a97a6bf988fc4a9b7614c8dc2e84be26cea08
mujcinov <xno1knowsx@gmail.com>
2026-02-06 11:20:48 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import './App.css';
 import EmblaCarousel from './EmblaCarousel';
+import Hamburger from 'hamburger-react';
+import { useState } from 'react';
+import { ImPhone } from "react-icons/im";
+import { IconContext } from "react-icons";
+
+
 function App() {
+  const [isOpen, setOpen] = useState(false)
   return (
     <>
       <div
@@ -9,19 +16,22 @@ function App() {
         <header className="top-bar">
          <img className='logo' src="/logo.png" alt="" />
 
-          <nav className="menu">
-            <a href="#">Destinacije</a>
-            <a href="#">Putovanja</a>
-            <a href="#">Kontakt</a>
-          </nav>
+        <nav className={`menu ${isOpen ? 'menu--open' : ''}`}>
+  <a href="#">Destinacije</a>
+  <a href="#">Putovanja</a>
+  <a href="#">Kontakt</a>
+</nav>
+
 
           <div className="actions">
             <div className="call">
-              <span className="call-icon">☎</span>
+              <a href="tel:+38762313986" className='call'>
+                <ImPhone className='call-icon' />
               <span className="call-text">Pozovite nas</span>
+              </a>
             </div>
             <button className="cta">Putujte s nama</button>
-            <div className="hamburger">☰</div>
+            <Hamburger className='hamburger' toggled={isOpen} toggle={setOpen}  />
           </div>
         </header>
       <EmblaCarousel />
