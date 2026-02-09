39882559210238000d3592f0a5b7913697f0f599
mujcinov <xno1knowsx@gmail.com>
2026-02-09 12:21:39 +0100

    update

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,16 @@
 import './SearchBar.css'
 import { useState } from 'react';
+import { ImSearch } from "react-icons/im";
+import { ImPlay3 } from "react-icons/im";
 export default function SearchBar() {
   const [open, setOpen] = useState(false)
   return (
     <div className={`wrapper ${open ? 'open' : ''}`}>
    
       <button className='button' onClick={() => setOpen(!open)}>
+        <ImSearch></ImSearch>
+        <p>Odaberite željenu destinaciju</p>
+        <ImPlay3></ImPlay3>
   </button>
 
   <div className={`dropdown-menu ${open ? 'open' : ''}`}>
