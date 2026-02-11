784aef31f4649477514c3f54e21fb5487332f2e9
mujcinov <xno1knowsx@gmail.com>
2026-02-11 11:32:52 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ <HashRouter>
+  <App />
+</HashRouter>
+
 );
