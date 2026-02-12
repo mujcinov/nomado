3712d759b3571b5e8bfd821c9585d4b8036456ec
mujcinov <xno1knowsx@gmail.com>
2026-02-12 12:08:47 +0100

    updated TripCard

--- src/Tours.jsx
+++ src/Tours.jsx
@@ -1,5 +1,6 @@
 import "./Tours.css";
 import TripCard from "./components/TripCard";
+import {destinations} from "./assets/destinations";
 
 function Tours() {
   return (
@@ -11,7 +12,9 @@ function Tours() {
           <h1>Putovanja</h1>
         </div>
         <div className="cards-section">
-            <TripCard></TripCard>
+           {destinations.map((trip) => (
+        <TripCard key={trip.id} trip={trip} />
+      ))}
         </div>
       </div>
     </div>
--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -1,30 +1,47 @@
 import "./TripCard.css";
+import { FaArrowCircleRight } from "react-icons/fa";
+import { RiDownloadCloud2Fill } from "react-icons/ri";
+import { IoInformationCircle } from "react-icons/io5";
 
-function TripCard(trip) {
+function TripCard({trip}) {
   return (
     <div className="trip-card">
-      <div className="image"></div>
+      <div className="image" style={{ backgroundImage: `url(${trip.image})` }}></div>
      <div className="info-section">
        <div className="tags">
-        <p>Istaknuto</p>
+        <p>{trip.tags}</p>
       </div>
-      <h1>Uzbekistan</h1>
+      <h2>{trip.name}</h2>
       <blockquote>I tako dalje nesto da ima vise teksta nmp bzvz</blockquote>
       <dl className="desc-list">
         <dt>Trajanje:</dt>
-        <dd>10 dana</dd>
+        <dd>{trip.duration}</dd>
         <dt>Datum polaska:</dt>
         <dd>11. april 2026</dd>
+        <dt>Datum povratka</dt>
+        <dd>20. april 2026</dd>
         <dt>Uključeno:</dt>
         <dd>Letovi - Hoteli - Vodič - Doručak</dd>
       </dl>
       <div className="price">
-        <p>od</p>
-        <h2>2490 KM</h2>
+        <p>Cijena od</p>
+        <div className="price-adjust">
+          <h2>2490 KM</h2>
+          <span>/po osobi</span>
+        </div>
       </div>
+      <div className="price-info">
+        <IoInformationCircle />
+        <span>Cijena se odnosi na privatnu dvokrevetnu sobu.</span></div>
       <div className="buttons">
-        <button className="itinirery-button">Preuzmi program</button>
-        <button className="details-button">Detalji putovanja</button>
+        <button className="itinerary-button">
+          <div className="download-icon"><RiDownloadCloud2Fill /></div>
+        <span>Preuzmi program</span>
+        </button>
+        <button className="details-button">
+          <div className="arrow-icon"><FaArrowCircleRight  /></div>
+        <span>Detalji putovanja</span>
+        </button>
       </div>
      </div>
     </div>
