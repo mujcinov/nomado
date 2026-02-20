1950519490eb67b095ed0cbb4e1467e2e7c54c50
mujcinov <xno1knowsx@gmail.com>
2026-02-20 10:44:35 +0100

    update

---
+++ src/components/Itinerary.jsx
@@ -0,0 +1,22 @@
+import "./Itinerary.css";
+import { useState } from "react";
+
+function Itinerary({ day = 1, details }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="itinerary-day">
+      <div className="itinerary-day-header" onClick={() => setIsOpen(!isOpen)}>
+        <span>Dan {day}</span>
+        <span className={`itinerary-arrow ${isOpen ? "open" : ""}`}>▼</span>
+      </div>
+      {isOpen && (
+        <div className="itinerary-day-details">
+          {details}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Itinerary;
--- src/components/SingleTrip.jsx
+++ src/components/SingleTrip.jsx
@@ -3,12 +3,30 @@ import { useParams } from "react-router-dom";
 import { destinations } from "../assets/destinations";
 import TripNavigation from "./TripNavigation";
 import { IoInformationCircle } from "react-icons/io5";
+import Itinerary from "./Itinerary";
 
 function SingleTrip() {
+
 const { unique } = useParams();
 const images = ["/malaysia.jpg", "/malaysia.jpg", "/malaysia.jpg", "/malaysia.jpg", 
   "/malaysia.jpg", "/malaysia.jpg", "/malaysia.jpg", "/malaysia.jpg", "/malaysia.jpg", "/malaysia.jpg"]
   const trip = destinations.find(t => t.unique === unique);
+    const formattedStartDate = new Date(trip.startDate).toLocaleDateString(
+    "de-DE",
+    {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    }
+  );
+   const formattedEndDate = new Date(trip.endDate).toLocaleDateString(
+    "de-DE",
+    {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    }
+  );
     return(
         <div className="single-trip">
       <div className="single-trip-topbar">
@@ -17,7 +35,7 @@ const images = ["/malaysia.jpg", "/malaysia.jpg", "/malaysia.jpg", "/malaysia.jp
       <div className="single-trip-feed">
         <h1>{trip.title}</h1>
         <div className="singletrip-tag"><p>{trip.tags}</p></div>
-        <div className="singletrip-date">od {trip.startDate} do {trip.endDate}</div>
+        <div className="singletrip-date">od {formattedStartDate} do {formattedEndDate}</div>
    <div className="gallery-scroll">
   {images.map((img, i) => (
     <img key={i} src={img} className={`img img-${i + 1}`} />
@@ -31,6 +49,7 @@ const images = ["/malaysia.jpg", "/malaysia.jpg", "/malaysia.jpg", "/malaysia.jp
         <IoInformationCircle />
         <span>Cijena se odnosi na privatnu dvokrevetnu sobu.</span></div>
 </div>
+<Itinerary></Itinerary>
       </div>
     </div>
     )
